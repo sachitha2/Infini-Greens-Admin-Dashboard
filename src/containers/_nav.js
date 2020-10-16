181b741d19ae7d216a15c16fa7c27d66c830c0d8
sachitha2 <shpsachitha@gmail.com>
2020-10-16 00:52:18 -0700

    init

---
+++ src/containers/_nav.js
@@ -0,0 +1,11 @@
+export default [
+  {
+    _tag: 'CSidebarNavItem',
+    name: 'Dashboard',
+    to: '/dashboard',
+    icon: 'cil-speedometer',
+    
+  },
+  //TODO   Navigation addd karanne methanin side naw bar ekata
+]
+
